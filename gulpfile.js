88ba2fa74c8de76cc2c6718df94181ef1c0284f2
cflatminor <cflatminor92@gmail.com>
2019-12-07 20:51:43 +0200

    add build & minify

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@
 const { src, watch, dest, series, parallel } = require('gulp');
 const sass = require('gulp-sass');
 const gulpConcat = require('gulp-concat');
+const uglify = require ('gulp-uglifyjs');
+const cssmin = require('gulp-cssmin');
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 const del = require('del');
@@ -18,15 +20,17 @@ function compileStyles() {
     return src('app/scss/index.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulpConcat('main.css'))
-        // .pipe(autoprefixer({
-        //   cascade: true
-        // }))
+        .pipe(autoprefixer({
+            cascade: true
+        }))
+        .pipe(cssmin())
         .pipe(dest('dist/'))
 }
 
 function compileJs () {
     return src('app/js/index.js')
         .pipe(gulpConcat('main.js'))
+        .pipe(uglify())
         .pipe(dest('dist/'))
 }
 
