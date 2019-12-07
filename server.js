cfa6d601e27b036cd02028fd6386e0a652f119f0
cflatminor <cflatminor92@gmail.com>
2019-12-07 04:28:29 +0200

    add server

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 'use strict';
 
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-// app.use(express.static(path.join(__dirname + '')));
+app.use(express.static(path.join(__dirname + '')));
 
-// app.get('/', (req, res) => res.sendFile('./index.html'));
+app.get('/', (req, res) => res.sendFile('./index.html'));
 
-// app.listen(port, function () {
-// 	console.log('listen on ' + port);
-// });
-
-app.listen(port);
+app.listen(port, function () {
+	console.log('listen on ' + port);
+});
