97a089a9605053b2ec58d725ec3fcfc477152c35
cflatminor <cflatminor92@gmail.com>
2019-12-07 04:24:14 +0200

    add server

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + ''));
+// app.use(express.static(path.join(__dirname + '')));
 
-app.get('/', (req, res) => res.sendFile('./index.html'));
+// app.get('/', (req, res) => res.sendFile('./index.html'));
 
-app.listen(port, function () {
-	console.log('listen on ' + port);
-});
+// app.listen(port, function () {
+// 	console.log('listen on ' + port);
+// });
+
+app.listen(port);
